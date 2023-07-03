f8c633f21dd96357d951a9e5f82afe0a12a47971
juniq <juniqlim@gmail.com>
2023-07-04 08:33:51 +0900

    test(article2): usecase test

--- article2/usecase/test/test_usecase.py
+++ article2/usecase/test/test_usecase.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
 
-from article2.usecase.repository import ArticleListRepository
-from article2.usecase.usecase import create_article, Request
+from article2.usecase.repository import save, find
+from article2.usecase.usecase import create_article, Request, find_article
 
 
 class Test(TestCase):
     def test_create_article(self):
-        create_article(ArticleListRepository().save, Request(title='title', description='description', body='body'))
+        create_article(save, Request(title='title', description='description', body='body'))
+        self.assertEquals(1, len(find_article(find)))
